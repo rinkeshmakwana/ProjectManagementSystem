dc23af884fdee4ec5e41bfe9cbbd6f0a8e9e037a
rinkeshmakwana <ricks14348@gmail.com>
2020-06-26 10:46:22 +0530

    class based view added

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import projects, new_project
+from .views import ProjectListView, new_project 
 
 
 urlpatterns = [
-    path('', projects , name='projects-list'),
+    path('', ProjectListView.as_view() , name='projects-list'),
     path('new-project/', new_project, name='new-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from .models import Project
-from users.models import UserProfile
+from django.views.generic import ListView
 from .forms import ProjectCreateForm
 
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'projects/projects.html'
+    # context_object_name = 'projects'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects_new'] = Project.objects.filter(status='1').order_by('-end_date')
+        context['projects_progress'] = Project.objects.filter(status='2').order_by('-end_date')
+        context['projects_completed'] = Project.objects.filter(status='3').order_by('-end_date')
+        context['projects_cancelled'] = Project.objects.filter(status='4').order_by('-end_date')
+        return context
 
 # Create your views here.
-def projects(request):
-    projects_list = Project.objects.all()    
-    context = {
-        'projects': projects_list,
-    }
-    return render(request, 'projects/projects.html', context)
+# def projects(request):
+#     projects_list = Project.objects.all()    
+#     context = {
+#         'projects': projects_list,
+#     }
+#     return render(request, 'projects/projects.html', context)
 
 
 def new_project(request):
